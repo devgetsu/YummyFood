1d50a50fcd337a226112fc5c627675c957bc9458
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-07-01 17:22:12 +0500

    DTOs done

---
+++ YummyFood.Domain/Entities/DTOs/ExternalLoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YummyFood.Domain.Entities.DTOs
+{
+    public class ExternalLoginDTO
+    {
+        public string Provider { get; set; }
+        public string ProviderKey { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
---
+++ YummyFood.Domain/Entities/DTOs/TokenDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YummyFood.Domain.Entities.DTOs
+{
+    public class TokenDTO
+    {
+        public string Token { get; set; }
+        public string Message { get; set; }
+        public bool isSuccess { get; set; }
+    }
+}
