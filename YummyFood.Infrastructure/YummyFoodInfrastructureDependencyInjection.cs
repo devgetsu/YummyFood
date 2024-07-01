3e9182078fd2ce935e311252e16d415284d6e430
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-07-01 11:00:09 +0500

    add to di

---
+++ YummyFood.Infrastructure/YummyFoodInfrastructureDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YummyFood.Infrastructure
+{
+    public static class YummyFoodInfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddYummyFoodInfrastructureDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+            return services;
+        }
+    }
+}
