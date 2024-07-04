4b0e29537717d860d9aa5a840f98a10f18fe28bc
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-07-04 15:59:00 +0500

    add Categrory crudd

---
+++ YummyFood.Application/UseCases/Categories/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YummyFood.Domain.Entities.DTOs;
+
+namespace YummyFood.Application.UseCases.Categories.Commands
+{
+    public class CreateCategoryCommand : IRequest<ResponseModel>
+    {
+        public string Name { get; set; }
+        public IFormFile Sticker { get; set; }
+    }
+}
