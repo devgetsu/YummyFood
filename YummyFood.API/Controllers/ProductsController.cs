9764ec5b1576fded6dcadd975adc080de363cbbc
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-07-04 15:22:03 +0500

    add product  vtrud+controller

---
+++ YummyFood.API/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YummyFood.Application.UseCases.Products.Commands;
+using YummyFood.Application.UseCases.Products.Queries;
+
+namespace YummyFood.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id, CancellationToken cancellationToken)
+        {
+            var command = new DeleteProductCommand { Id = id };
+
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductByIdAsync(long id, CancellationToken cancellationToken)
+        {
+            var query = new GetProductByIdQuery() { Id = id };
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ YummyFood.Application/UseCases/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YummyFood.Domain.Entities.DTOs;
+
+namespace YummyFood.Application.UseCases.Products.Commands
+{
+    public class CreateProductCommand : IRequest<ResponseModel>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile Picture { get; set; }
+        public long CategoryId { get; set; }
+    }
+}
